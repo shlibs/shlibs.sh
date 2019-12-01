83bc32ac21357d110b511fda61fb741989ae9e28
SDRausty <27742457+sdrausty@users.noreply.github.com>
2019-12-01 18:14:53 -0500

    commit 321081412 in 2019

--- buildAPKs/maintenance/up.sh
+++ buildAPKs/maintenance/up.sh
@@ -6,8 +6,8 @@
 set -eu 
 RDR="$HOME/buildAPKs" # define root directory
 cd "$RDR" # change directory to root directory
-git pull # run ` git pull ` to update the the local copy of git repository https://github.com/BuildAPKs/buildAPKs to the newest version 
-rm -f scripts/bash/shlibs/.git scripts/sh/shlibs/.git  # remove automatically generated submodule .git files
+git pull # run ` git pull ` to update the the local copy of remote git repository https://github.com/BuildAPKs/buildAPKs to the newest version on device 
+rm -f scripts/bash/shlibs/.git scripts/sh/shlibs/.git  # remove automatically generated submodule .git files, which were created through the process of cloning and updating git repositories
 sleep 0.42 # intensify latency support on fast networks;  ` grep -hC 4 -r sleep ~/buildAPKs/scripts ` shows additional latency usage of ` sleep ` in BuildAPKs once BuildAPKs is installed
 git submodule update --init --recursive --remote scripts/bash/shlibs # ` git submodule help ` and https://git-scm.com/book/en/v2/Git-Tools-Submodules have more information.  The source code for this book is hosted at https://github.com/progit/progit2
 sleep 0.42 # increase latency support on fast networks;  See ` grep -hC 4 -r sleep ~/buildAPKs/scripts ` for complementary latency applications of ` sleep ` when BuildAPKs is setup.  You can use https://raw.githubusercontent.com/BuildAPKs/buildAPKs/master/scripts/bash/init/setup.buildAPKs.bash to set ~/buildAPKs up on device with ` curl -OL https://raw.githubusercontent.com/BuildAPKs/buildAPKs/master/scripts/bash/init/setup.buildAPKs.bash ; bash setup.buildAPKs.bash `.
