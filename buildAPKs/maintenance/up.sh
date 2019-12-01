75ba9cca39cb3156db62c35410f24fc08731753b
S D Rausty <27742457+SDRausty@users.noreply.github.com>
2019-12-01 16:39:58 -0500

    Update up.sh

--- buildAPKs/maintenance/up.sh
+++ buildAPKs/maintenance/up.sh
@@ -7,8 +7,8 @@ RDR="$HOME/buildAPKs" # define root directory
 cd "$RDR" # change directory to root directory
 git pull
 rm -f scripts/bash/shlibs/.git scripts/sh/shlibs/.git  # remove .git files
-sleep 0.42 # added for network latency support on fast networks; see ` grep -hC 4 -r ~/buildAPKs/scripts ` for more applications.
+sleep 0.42 # added for network latency support on fast networks; see ` grep -hC 4 -r ~/buildAPKs/scripts ` for more applications when installed.
 git submodule update --init --recursive --remote scripts/bash/shlibs # git submodule help has more information
-sleep 0.42 # added network latency support on fast networksnetworks; see ` grep -hC 4 -r ~/buildAPKs/scripts ` for more allocations.
+sleep 0.42 # added network latency support on fast networksnetworks; see ` grep -hC 4 -r ~/buildAPKs/scripts ` for more allocations when setup.
 git submodule update --init --recursive --remote scripts/sh/shlibs # git submodule help has more information
 # up.sh EOF
