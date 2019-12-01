f4e78f00ec1cd4b0262df55a36640c00d7827eb2
SDRausty <27742457+sdrausty@users.noreply.github.com>
2019-12-01 17:21:06 -0500

    commit 135302733 in 2019

--- buildAPKs/maintenance/up.sh
+++ buildAPKs/maintenance/up.sh
@@ -1,14 +1,15 @@
 #!/bin/env sh
 # Copyright 2019 (c) all rights reserved by S D Rausty;  See LICENSE  
 # https://sdrausty.github.io hosted courtesy https://pages.github.com
+# Run to update ~/buildAPKs and modules to the newest version
 #####################################################################
 set -eu 
 RDR="$HOME/buildAPKs" # define root directory
 cd "$RDR" # change directory to root directory
-git pull
-rm -f scripts/bash/shlibs/.git scripts/sh/shlibs/.git  # remove .git files
-sleep 0.42 # added for network latency support on fast networks; See ` grep -hC 4 -r ~/buildAPKs/scripts ` for more latency applications when BuildAPKs is installed
-git submodule update --init --recursive --remote scripts/bash/shlibs # ` git submodule help ` has more information
-sleep 0.42 # added network latency support on fast networksnetworks; See ` grep -hC 4 -r ~/buildAPKs/scripts ` for more latency applications when BuildAPKs is setup
-git submodule update --init --recursive --remote scripts/sh/shlibs # ` git submodule help ` has more information
+git pull # run ` git pull ` 
+rm -f scripts/bash/shlibs/.git scripts/sh/shlibs/.git  # remove automatically generated submodule .git files
+sleep 0.42 # intensify latency support on fast networks;  ` grep -hC 4 -r sleep ~/buildAPKs/scripts ` shows additional latency usage of ` sleep ` in BuildAPKs once BuildAPKs is installed
+git submodule update --init --recursive --remote scripts/bash/shlibs # ` git submodule help ` and https://git-scm.com/book/en/v2/Git-Tools-Submodules have more information.  This book is hosted at https://github.com/progit/progit2
+sleep 0.42 # increase latency support on fast networks;  See ` grep -hC 4 -r sleep ~/buildAPKs/scripts ` for complementary latency applications of ` sleep ` when BuildAPKs is setup
+git submodule update --init --recursive --remote scripts/sh/shlibs # ` git submodule help ` and https://git-scm.com/book/en/v2/Git-Tools-Submodules have more information.  This book is hosted at https://github.com/progit/progit2
 # up.sh EOF
