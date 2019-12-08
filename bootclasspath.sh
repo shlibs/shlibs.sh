a70c9c3d6dbc6b1cc5e9c453170a1f7867fe85a9
SDRausty <27742457+sdrausty@users.noreply.github.com>
2019-12-07 20:11:22 -0500

    change 308630939 on 20191207

--- bootclasspath.sh
+++ bootclasspath.sh
@@ -1,8 +1,8 @@
 #!/bin/env sh
 # Copyright 2019 (c) all rights reserved by S D Rausty;  see  LICENSE  
 # https://sdrausty.github.io hosted courtesy https://pages.github.com
-# R1 https://github.com/termux/termux-packages/issues?q=BOOTCLASSPATH
-# Refer2: https://github.com/termux/termux-app/issues?q=BOOTCLASSPATH
+# Refer: https://github.com/termux/termux-app/search?&q=BOOTCLASSPATH
+# R2 https://github.com/termux/termux-packages/search?q=BOOTCLASSPATH
 # Export to shell usage: ` . bootclasspath.sh ` # the dot is required
 #####################################################################
 set -eu
