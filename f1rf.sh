3503295935118b79eebcd5ca0c2e96bf1564f3a4
SDRausty <27742457+sdrausty@users.noreply.github.com>
2019-12-12 11:42:02 -0500

    change 170025601 on 2019.12.12

--- f1rf.sh
+++ f1rf.sh
@@ -2,20 +2,20 @@
 set -eu
 _MAINDCTAR_ () {
 	O1DIR=$PWD
-	h=$(ls)
-	for RMRFDIR1 in $h
+	FDIRL=$(ls)
+	for RMRFDIR1 in $FDIRL
 	do
-		cd $RMRFDIR1 && find . -maxdepth 1 -type d -exec rm -rf {} \;
+		find $RMRFDIR1 -maxdepth 1 -type d -exec rm -rf {} \;
 		printf "%s\\n" "${PWD##*/}"
 		cd $O1DIR
 	done
 }
 
 _PRINTHDCT_ () {
-	printf "%s\\n%s\\n" "Options for \` ${0##*/} \` are:" "See \` cat $0 \` for more information."
-	grep -w "cd\ " "$0" | head -1 
+	printf "%s\\n" "See \` cat $0 \` for more information as \` ${0##*/} \` removes directories as requested with the following command:"
+	grep -w find "$0" | head -1 
 	grep -w "elif \[" "$0" | awk '{print $5"	"$8" "$9" "$10" "$11" "$12" "$13}'
-	printf "%s\\n" "\` ${0##*/} \` should be run with an option as \` ${0##*/} \` removes directories as requested."
+	printf "%s\\n" "\` ${0##*/} \` should be run with an option as \` ${0##*/} \` removes directories."
 }
 
 [ -z "${1:-}" ] && _PRINTHDCT_ || _MAINDCTAR_ "$@" 
