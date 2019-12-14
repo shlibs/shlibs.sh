4cd84dbbfe77297d64fb23df809beb4fd2676500
SDRausty <27742457+SDRausty@users.noreply.github.com>
2019-12-14 15:35:16 -0500

    commit 954377112 on 20191214

--- f1rmrfd.sh
+++ f1rmrfd.sh
@@ -6,10 +6,10 @@
 set -eu
 _MAINDCTAR_ () {
 	O1DIR=$PWD
-	FDIRL=$(ls)
+	FDIRL=$(ls --color=never)
 	for RMRFDIR1 in $FDIRL
 	do
-		find $RMRFDIR1 -maxdepth 1 -type d -exec rm -rf {} \;
+		find $RMRFDIR1 -maxdepth 1 -type d -exec rm -rf {} \; ||:
 		printf "%s\\n" "${PWD##*/}"
 		cd $O1DIR
 	done
