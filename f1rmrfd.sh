25def4c5535a30cef6b7001c268123eeb2396583
SDRausty <27742457+SDRausty@users.noreply.github.com>
2019-12-23 17:57:52 -0500

    commit 908894153 on 20191223

--- f1rmrfd.sh
+++ f1rmrfd.sh
@@ -1,4 +1,4 @@
-#!/usr//bin/env sh
+#!/usr/bin/env sh
 # Copyright 2019 (c)  all rights reserved by S D Rausty;  see LICENSE  
 # https://sdrausty.github.io hosted courtesy https://pages.github.com
 # This file:  ` f1rmrfd.sh `  deletes directories at find -maxdepth 1 
