6a4173650027442c116195a47252fdf2ccaac2c1
SDRausty <27742457+SDRausty@users.noreply.github.com>
2019-12-24 14:08:25 -0500

    commit 138589988 on 20191224

--- buildAPKs/maintenance/up.sh
+++ buildAPKs/maintenance/up.sh
@@ -11,7 +11,7 @@ sleep 0.$(shuf -i 24-72 -n 1)	# add device latency support;  Commands like this 
 
 rm -f opt/db/.git scripts/bash/github/.git scripts/bash/shlibs/.git scripts/sh/shlibs/.git ||:	# remove automatically generated submodule .git files which were created through the process of cloning and updating git repositories as submodules.
 
-git submodule update --init --recursive --remote opt/db || ([ -d "$RDR"/opt/db ] && cd "$RDR"/opt/db && git pull && cd "$RDR") ||:	# The command ` git submodule help ` and the book https://git-scm.com/book/en/v2/Git-Tools-Submodules have more information about git submodules.
+git submodule update --init --recursive --remote opt/db	# The command ` git submodule help ` and the book https://git-scm.com/book/en/v2/Git-Tools-Submodules have more information about git submodules.
 
 sleep 0.$(shuf -i 24-72 -n 1)	# enhance network latency support on fast networks;  ` grep -hC 4 -r sleep ~/buildAPKs/scripts ` shows additional latency usage of ` sleep ` in BuildAPKs once BuildAPKs is installed.  Commands like this script, and ` build.github.bash ` can send many requests.  This can lead to network packet collisions on a fast device that is connected to a fast network, which in turn causes excessive network latency.
 
