7e0d7f2bb83e4ed2d9a035c661ca678be8475e6e
SDRausty <27742457+SDRausty@users.noreply.github.com>
2020-07-18 06:57:59 -0400

    commit 432943751 on 20200718

---
+++ buildAPKs/inst.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env sh
+# Copyright 2020 (c) all rights reserved by S D Rausty; See LICENSE
+# File `inst.sh` is under development
+#####################################################################
+set -eu 
+_INST_() {
+# check for neccessary commands: begin
+COMMS="$1"
+COMMANDR="$(command -v au)" || (printf "%s\\n\\n" "$STRING1") 
+COMMANDIF="${COMMANDR##*/}"
+STRING1="COMMAND \`au\` enables rollback, available at https://wae.github.io/au/ IS NOT FOUND: Continuing... "
+STRING2="Cannot update ~/${RDR##*/} prerequisite: Continuing..."
+PKGS="$2"
+_INPKGS_() {
+	if [ "$COMMANDIF" = au ]
+	then 
+		au "$PKGS" || printf "%s\\n" "$STRING2"
+	else
+		apt install "$PKGS" || printf "%s\\n" "$STRING2"
+	fi
+}
+for COMMA in "$COMMS"
+do
+	COMMANDP="$(command -v "$COMMA")" || printf "Command %s not found: Continuing...\\n" "$COMMA" # test if command exists
+	COMMANDPF="${COMMANDP##*/}"
+	if [ "$COMMANDPF" != "$COMMA" ]
+	then 
+		printf "%s\\n" "Beginning buildAPKs \`$3\` setup:"
+		_INPKGS_
+	fi
+done
+# check for neccessary commands: end
+}
+# inst.sh EOF
