eeb82a61e2aac86d58f63dec337c9792818ba5d1
SDRausty <27742457+SDRausty@users.noreply.github.com>
2021-08-09 01:03:18 -0400

    commit 657444611 on 20210809

--- buildAPKs/maintenance/up.sh
+++ buildAPKs/maintenance/up.sh
@@ -7,8 +7,8 @@ set -eu
 
 _CSLIST_ () {	# create checksum file RDR/.conf/sha512.sum and compare with RDR/sha512.sum
 	FAUTH="DOSO DOSON DRLIM EXTSTDO GAUTH LIBAUTH QUIET RDR" # exemption file list
-	CSLICK="1"	# false: files have not changed
-	GEFAUTH="-e .gitmodules"	# grep -e use matching string
+	CSLICK=1	# false: files have not changed
+	GEFAUTH="-v .gitmodules"	# grep -v select non-matching lines
 	cd "$RDR"/.conf/
 	sha512sum $FAUTH > sha512.sum	# create checksum file RDR/.conf/sha512.sum
 	for QAUTH in $FAUTH	# each element in FAUTH
@@ -22,9 +22,10 @@ _CSLIST_ () {	# create checksum file RDR/.conf/sha512.sum and compare with RDR/s
 		fi
 	done
 	cd "$RDR"
+		grep -v -e ./setup.buildAPKs.bash "$GEFAUTH" "$RDR/sha512.sum" > "$RDR/var/tmp/${0##*/}.$$.tmp"
 	if [ "$CSLICK" = 0 ]
 	then
-		grep -v -e ./setup.buildAPKs.bash $GEFAUTH "$RDR/sha512.sum" > "$RDR/var/tmp/${0##*/}.$$.tmp"
+		grep -v -e ./setup.buildAPKs.bash "$GEFAUTH" "$RDR/sha512.sum" > "$RDR/var/tmp/${0##*/}.$$.tmp"
 	else
 		grep -v ./setup.buildAPKs.bash "$RDR/sha512.sum" > "$RDR/var/tmp/${0##*/}.$$.tmp"
 	fi
@@ -57,19 +58,19 @@ _PRT_ () {	# print message with no trialing newline
 }
 
 _UP_ () {	# add or update git submodule repository
-	( git submodule update --depth 1 --recursive --remote "$IMFSTRG" || git submodule add --depth 1 "$MRASTRG" "$IMFSTRG" ) && cd "$RDR/$IMFSTRG" && _PRCS_ && cd "$RDR" || _PESTRG_	# the command ` git submodule help ` and the book https://git-scm.com/book/en/v2/Git-Tools-Submodules have more information about git submodules.
-	sleep 0."$(shuf -i 24-72 -n 1)"	# enhance device and network latency support on fast networks;  See ` grep -hC 4 -r sleep ~/buildAPKs/scripts ` for complementary latency applications of ` sleep ` when BuildAPKs is installed.  You can use https://raw.githubusercontent.com/BuildAPKs/buildAPKs/master/setup.buildAPKs.bash to set ~/buildAPKs up on device with ` curl -OL https://raw.githubusercontent.com/BuildAPKs/buildAPKs/master/setup.buildAPKs.bash ; bash setup.buildAPKs.bash `.  It appears that a little sleep can go a long way in reducing network collisions on fast networks.
+	( ( git submodule update --depth 1 --recursive --remote "$IMFSTRG" || git submodule add --depth 1 "$MRASTRG" "$IMFSTRG" ) && cd "$RDR/$IMFSTRG" && _PRCS_ && cd "$RDR" ) || _PESTRG_	# the command ` git submodule help ` and the book https://git-scm.com/book/en/v2/Git-Tools-Submodules have more information about git submodules.
+	sleep 0.$(shuf -i 24-72 -n 1)	# enhance device and network latency support on fast networks;  See ` grep -hC 4 -r sleep ~/buildAPKs/scripts ` for complementary latency applications of ` sleep ` when BuildAPKs is installed.  You can use https://raw.githubusercontent.com/BuildAPKs/buildAPKs/master/setup.buildAPKs.bash to set ~/buildAPKs up on device with ` curl -OL https://raw.githubusercontent.com/BuildAPKs/buildAPKs/master/setup.buildAPKs.bash ; bash setup.buildAPKs.bash `.  It appears that a little sleep can go a long way in reducing network collisions on fast networks.
 }
 
-_PRNT_ "Script ${0##*/}: STARTED..."
 WSTRING="Warning ${0##*/}; Continuing...  "	# define WSTRING warning message
 RDR="$HOME/buildAPKs"		# define root directory
 SIAD="https://github.com"	# define site address
-SIADS="$SIAD/BuildAPKs"	# define login
+SIADS="$SIAD/BuildAPKs"	# define remote login
+_PRNT_ "Script ${0##*/}: STARTED..."
 cd "$RDR"		# change directory to root directory
 git pull --ff-only	# update local git repository to the newest version
-_CSLIST_ || _PESTRG_	# run either functions _CSLIST_ or _PESTRG_
-sleep 0."$(shuf -i 24-72 -n 1)"	# add device and network latency support;  Commands like this script can request many read write operations.  The sleep plus shuf commands cause this script to wait for a short pseudo random period of time.  This can ease excessive device latency when running these build scripts.
+_CSLIST_ || _PESTRG_	# run function _PESTRG_ if function _CSLIST_ errs
+sleep 0.$(shuf -i 24-72 -n 1)	# add device and network latency support;  Commands like this script can request many read write operations.  The sleep plus shuf commands cause this script to wait for a short pseudo random period of time.  This can ease excessive device latency when running these build scripts.
 rm -f opt/api/github/.git opt/db/.git scripts/bash/shlibs/.git scripts/sh/shlibs/.git || _PESTRG_	# remove automatically generated submodule .git files which were created through the process of cloning and updating git repositories as submodules
 IMFSTRG="opt/api/github"	# define install module folder
 MRASTRG="$SIADS/buildAPKs.github"	 # define module repository site
@@ -84,6 +85,6 @@ _UP_
 IMFSTRG="scripts/sh/shlibs"
 MRASTRG="$SIADS/shlibs.sh"
 _UP_
-_PRT_ "Removing '.git' files: " && find "$RDR/sources/" -maxdepth 2 -type f -name .git -delete && _PRNT_ "DONE" || _PESTRG_
+( _PRT_ "Removing '.git' files: " && find "$RDR/sources/" -maxdepth 2 -type f -name .git -delete && _PRNT_ "DONE" ) || _PESTRG_
 _PRNT_ "Script ${0##*/}: DONE"
 # up.sh EOF
