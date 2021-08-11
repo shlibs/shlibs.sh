196dfdae6fe28b7d1e66c83e6c72ce01f519707f
SDRausty <27742457+SDRausty@users.noreply.github.com>
2021-08-11 00:39:56 -0400

    commit 195365787 on 20210811

--- buildAPKs/maintenance/up.sh
+++ buildAPKs/maintenance/up.sh
@@ -71,6 +71,7 @@ cd "$RDR"		# change directory to root directory
 git pull --ff-only	# update local git repository to the newest version
 _CSLIST_ || _PESTRG_	# run function _PESTRG_ if function _CSLIST_ errs
 sleep 0.$(shuf -i 24-72 -n 1)	# add device and network latency support;  Commands like this script can request many read write operations.  The sleep plus shuf commands cause this script to wait for a short pseudo random period of time.  This can ease excessive device latency when running these build scripts.
+sed -i '/gitmodules/d' sha512.sum
 rm -f opt/api/github/.git opt/db/.git scripts/bash/shlibs/.git scripts/sh/shlibs/.git || _PESTRG_	# remove automatically generated submodule .git files which were created through the process of cloning and updating git repositories as submodules
 IMFSTRG="opt/api/github"	# define install module folder
 MRASTRG="$SIADS/buildAPKs.github"	 # define module repository site
