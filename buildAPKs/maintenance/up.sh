2c1d9dd58ae056a83fe696325f6890afba577173
SDRausty <27742457+SDRausty@users.noreply.github.com>
2021-09-08 16:28:14 -0400

    commit 063573277 on 20210908

--- buildAPKs/maintenance/up.sh
+++ buildAPKs/maintenance/up.sh
@@ -74,7 +74,7 @@ RDR="$HOME/buildAPKs"		# define root directory
 SIAD="https://github.com"	# define site address
 SIADS="$SIAD/BuildAPKs"	# define remote login
 cd "$RDR"	# change directory to root directory
-git pull || ( git add && git commit && git pull ) || ( printf "%s\\n"  "Please study the output.  Directory '~/${RDR##*/}/stash' can be used to store files if 'error: Your local changes to the following files would be overwritten by merge:' is found. Then please use this ${0##*/} to update ~/${RDR##*/} to the most recent version published." && exit 204 )	# update local git repository to the newest version published
+git pull || ( git add && git commit && git pull ) || { printf "%s\\n"  "Please study the output of the first error.  If 'error: Your local changes to the following files would be overwritten by merge:' is found, directory '~/${RDR##*/}/stash' can be used to store files.  Then please use '${0##*/}' to update ~/${RDR##*/} to the most recent version published." ; _PRNT_ "Script ${0##*/}: DONE" ; exit 204 ; }	# attempt to update local git repository to the newest version published
 _CSLIST_ || _PESTRG_	# run function _PESTRG_ if function _CSLIST_ errs
 sleep 0.$(shuf -i 24-72 -n 1)	# add device and network latency support;  Commands like this script can request many read write operations.  The sleep plus shuf commands cause this script to wait for a short pseudo random period of time.  This can ease excessive device latency when running these build scripts.
 if "$COMDGREP" gitmodules sha512.sum 1>/dev/null
