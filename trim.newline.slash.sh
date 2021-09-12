e59a6459f4e7641a2808823422b668931d052d92
SDRausty <27742457+SDRausty@users.noreply.github.com>
2021-09-12 14:58:09 -0400

    commit 690733506 on 20210912

---
+++ trim.newline.slash.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+# Copyright 2021 (c)  all rights reserved by S D Rausty;  see LICENSE
+# https://sdrausty.github.io hosted courtesy https://pages.github.com
+# strip leading whitespaces, then trim slashes with newlines
+#####################################################################
+set -eu
+_SLWTTSWN_() {
+awk '{ sub(/^[ \t]+/, ""); print }' "$1" | awk '{if (sub(/\\$/,"")) printf "%s", $0; else print $0}'
+}
+_SLWTTSWN_ "$@"
+# trim.newline.slash.sh EOF
