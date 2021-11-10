39ecf32b5d9dfcb0cbc966e4f599eb7d6b5f0e03
SDRausty <27742457+SDRausty@users.noreply.github.com>
2021-11-10 13:17:04 -0500

    commit 365851143 on 20211110

--- emptyimage.sh
+++ emptyimage.sh
@@ -3,7 +3,6 @@
 FJPG="$(find . -name "*jpg")"
 for FMMX in $FJPG
 do
-printf '%s\n' "$FMMX"
-printf '%s\n' "$(convert "$FMMX" -format '%[mean] %[max]' info:- | awk '{print $1/$2}')"
+printf 'File %s mean/max = %s\n' "${FMMX##*/}'s" "$(convert "$FMMX" -format '%[mean] %[max]' info:- | awk '{print $1/$2}')"
 done
 # emptyimage.sh EOF
